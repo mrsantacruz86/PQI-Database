0112b6082ec29e3ac19aa6c84004bf279af44f8f
Nelson Diaz <abnerdc86@gmail.com>
2018-12-31 16:38:24 -0500

    Fix loging and signup pages.

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import BarAndMenu from '../components/BarAndMenu';
 import LoginForm from '../components/LoginForm';
-
-
-const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    height: '100vh',
-    alignItems: 'center',
-  },
-
-});
 
 class Dashboard extends React.Component {
 
   render() {
-    const { classes } = this.props;
-
     return (
       <BarAndMenu pageName="Login">
-        <div className={classes.appBarSpacer} />
         <LoginForm />
       </BarAndMenu>
     );
@@ -37,4 +18,4 @@ Dashboard.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Dashboard);
+export default (Dashboard);
--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,38 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import withStyles from '@material-ui/core/styles/withStyles';
-import RegisterForm from '../components/RegisterForm';
 import BarAndMenu from '../components/BarAndMenu';
+import RegisterForm from '../components/RegisterForm';
 
-const styles = theme => ({
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    height: '100vh',
-    alignItems: 'center',
-  },
-});
+class SingUpPage extends React.Component {
 
-function SignUpPage(props) {
-  const { classes } = props;
-
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <BarAndMenu />
-      <main className={classes.content}>
+  render() {
+    return (
+      <BarAndMenu pageName="Login">
         <RegisterForm />
-      </main>
-    </div>
-  );
+      </BarAndMenu>
+    );
+  }
 }
 
-SignUpPage.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-SignUpPage.contextTypes = {
-  router: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(SignUpPage);
+export default (SingUpPage);
