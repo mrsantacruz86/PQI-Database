f3d911c630624a75d49992fcace73573c2a63503
Nelson Diaz <abnerdc86@gmail.com>
2019-01-25 02:15:00 -0500

    Solved the issues with the audit items loading delay.

--- client/src/components/HouseAuditForm.jsx
+++ client/src/components/HouseAuditForm.jsx
@@ -5,23 +5,19 @@ import { compose } from 'redux';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import MenuItem from '@material-ui/core/MenuItem';
-// import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import FormHelperText from '@material-ui/core/FormHelperText';
 import Checkbox from '@material-ui/core/Checkbox';
 import moment from 'moment';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import {
   saveHouseAudit,
 } from '../actions/houseAuditActions';
-// import items from '../utils/Items';
 
 const styles = theme => ({
   button: {
@@ -76,105 +72,106 @@ class NewHouseAudit extends Component {
 
   render() {
     const { classes } = this.props;
-    const { houseList, houseAuditTemplate} = this.props;
+    const { houseList, houseAuditTemplate } = this.props;
     // console.log(hItems);
 
     return (
       <div>
-          <form onSubmit={this.handleSubmit} >
-            <TextField
-              required
-              name="date"
-              type="date"
-              id="audit-date"
-              label="Audit Date"
-              value={this.state.date}
-              className={classes.textField}
-              margin="normal"
-              onChange={this.handleChange}
-              InputLabelProps={{ shrink: true }}
-            />
-            <TextField
-              required
-              select
-              name="house"
-              id="house"
-              label="House Number"
-              value={this.state.house}
-              className={classes.textField}
-              margin="normal"
-              onChange={this.handleChange}
-              InputLabelProps={{ shrink: true }}
-              SelectProps={{
-                // native: true,
-                MenuProps: {
-                  className: classes.menu,
-                },
-              }}
-            >
-              {houseList.map(option => (
-                <MenuItem key={option.number} value={option.number}>
-                  {option.number}
-                </MenuItem>
-              ))}
-            </TextField>
-            <Typography variant="h6" className={classes.subtitle}>
-              Household Audit
+        <form onSubmit={this.handleSubmit} >
+          <TextField
+            required
+            name="date"
+            type="date"
+            id="audit-date"
+            label="Audit Date"
+            value={this.state.date}
+            className={classes.textField}
+            margin="normal"
+            onChange={this.handleChange}
+            InputLabelProps={{ shrink: true }}
+          />
+          <TextField
+            required
+            select
+            name="house"
+            id="house"
+            label="House Number"
+            value={this.state.house}
+            className={classes.textField}
+            margin="normal"
+            onChange={this.handleChange}
+            InputLabelProps={{ shrink: true }}
+            SelectProps={{
+              // native: true,
+              MenuProps: {
+                className: classes.menu,
+              },
+            }}
+          >
+            {houseList.map(option => (
+              <MenuItem key={option.number} value={option.number}>
+                {option.number}
+              </MenuItem>
+            ))}
+          </TextField>
+          <Typography variant="h6" className={classes.subtitle}>
+            Household Audit
             </Typography>
-            {/* <FormLabel component="legend">Household Audit</FormLabel> */}
-            <hr />
-            <Grid container>
-              {/* {console.log(houseAuditTemplate)} */}
-              {houseAuditTemplate.map(item => (
+          {/* <FormLabel component="legend">Household Audit</FormLabel> */}
+          <hr />
+          <Grid container>
+            {/* {console.log(houseAuditTemplate)} */}
+            {!(houseAuditTemplate.length > 0) ?
+              <CircularProgress />
+              :
+              houseAuditTemplate.map(item => (
                 <Grid item xs={12} sm={6} md={4} lg={3} key={item.itemId}>
                   <FormControl fullWidth /* component="fieldset" */ className={classes.formControl} >
-                    <FormGroup>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={this.state.houseAudit[item.name]}
-                            value={item.name}
-                            onChange={this.handleSelect}
-                          />
-                        }
-                        label={item.label}
-                      />
-                    </FormGroup>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={this.state.houseAudit[item.name]}
+                          value={item.name}
+                          onChange={this.handleSelect}
+                        />
+                      }
+                      label={item.label}
+                    />
                     {/* <FormHelperText>Be careful</FormHelperText>  */}
                   </FormControl>
                 </Grid>
               ))}
-            </Grid>
+          </Grid>
 
 
-            <Typography variant="h6" className={classes.subtitle} >
-              Maintenance Audit
+          <Typography variant="h6" className={classes.subtitle} >
+            Maintenance Audit
             </Typography>
-            <hr />
-            <Grid
-              container
-              spacing={24}
-              justify="flex-end"
+          <hr />
+          <Grid
+            container
+            spacing={24}
+            justify="flex-end"
+          >
+            <Button
+              type="reset"
+              variant="contained"
+              color="secondary"
+              className={classes.button}
             >
-              <Button
-                type="reset"
-                variant="contained"
-                color="secondary"
-                className={classes.button}
-              >
-                Cancel
+              Cancel
               </Button>
 
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                className={classNames(classes.submit, classes.button)}
-              >
-                Submit
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classNames(classes.submit, classes.button)}
+            >
+              Submit
               </Button>
-            </Grid>
-          </form>
+          </Grid>
+        </form>
       </div>
     );
   }
