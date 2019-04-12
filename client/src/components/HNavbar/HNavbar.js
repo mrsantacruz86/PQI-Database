78ef3ea4471fc93a295d6815b2ae089df09f481e
Nelson Diaz <abnerdc86@gmail.com>
2019-04-12 19:08:17 -0400

    Working on navbar push #1

--- client/src/components/HNavbar/HNavbar.js
+++ client/src/components/HNavbar/HNavbar.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
 import "./HNavbar.css";
 
 class HNavbar extends Component {
 
   render() {
+    const selected = (path) => {
+      if (path !== window.location.pathname) {
+        return "";
+      } else {
+        return " active";
+      }
+    }
+
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <Link className="navbar-brand" to="/">PQI TOOLS</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <ul className="navbar-nav mr-auto">
+              <li className={`nav-item${selected("/")}`}>
+                <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
+              <li className={`nav-item${selected("/dashboard")}`}>
+                <Link className="nav-link" to="/dashboard">Dashboard</Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
+              <li className={`nav-item${selected("/house-audits")}`}>
+                <Link className="nav-link" to="/house-audits">House Audits</Link>
               </li>
             </ul>
+            <Link className="btn btn-outline-light btn-sm" to="/login">
+              Login
+            </Link>
+            <div className="nav-item dropdown">
+              <a class="nav-link text-light" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" id="userMenu">
+                <i className="fas fa-user-circle fa-lg"></i>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="userMenu">
+                {/* <span class="dropdown-item-text">Dropdown item text</span> */}
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </div>
+
+
           </div>
         </nav>
       </div>
@@ -30,4 +56,6 @@ class HNavbar extends Component {
   }
 }
 
-export default (HNavbar);
+const mapStateToProps = (state) => ({ ...state });
+
+export default connect(mapStateToProps)(HNavbar);
--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 import { login } from '../../actions/appActions';
 import './LoginForm.css'
@@ -39,41 +40,45 @@ class LoginForm extends Component {
   render() {
     const { auth } = this.props.app;
     if (auth) { //checks if the user is authenticated and redirects to home
-
-    }
+      return (
+        <Redirect to="/" />
+      )
+    };
     return (
       <React.Fragment >
-        <div className="login-form">
-          <div className="user-icon text-primary">
-            <i className="fas fa-user-circle"></i>
+        <div className="col">
+          <div className="login-form">
+            <div className="user-icon text-primary">
+              <i className="fas fa-user-circle"></i>
+            </div>
+            <h2>Sign in</h2>
+
+            <form className="form-signin" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label for="username">Username or email</label>
+                <input type="text"
+                  className="form-control"
+                  id="username"
+                  placeholder="Enter username or email"
+                  name="username"
+                  onChange={this.handleInputChange}
+                  value={this.state.user.username} />
+              </div>
+
+              <div className="form-group">
+                <label for="password">Password</label>
+                <input type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={this.handleInputChange}
+                  value={this.state.user.password}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">Sign in</button>
+            </form>
           </div>
-          <h2>Sign in</h2>
-
-          <form className="form-signin" onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label for="username">Username or email</label>
-              <input type="text"
-                className="form-control"
-                id="username"
-                placeholder="Enter username or email"
-                name="username"
-                onChange={this.handleInputChange}
-                value={this.state.user.username} />
-            </div>
-
-            <div className="form-group">
-              <label for="password">Password</label>
-              <input type="password"
-                className="form-control"
-                id="password"
-                placeholder="Password"
-                name="password"
-                onChange={this.handleInputChange}
-                value={this.state.user.password}
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">Sign in</button>
-          </form>
         </div>
       </React.Fragment>
     );
