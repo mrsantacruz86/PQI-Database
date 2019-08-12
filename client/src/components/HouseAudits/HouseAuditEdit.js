20c26ee51d3c0224084ce5e56fd83f573dfddb85
Nelson Diaz <abnerdc86@gmail.com>
2019-08-12 03:27:17 -0400

    create audit form

--- client/src/components/HouseAudits/HouseAuditEdit.js
+++ client/src/components/HouseAudits/HouseAuditEdit.js
@@ -1,11 +1,93 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
+import history from '../../history';
+import { connect } from 'react-redux';
 
-const HouseAuditEdit = () => {
+// Data json files
+import { editHouseAudit, fetchHouseAudit } from '../../actions/houseAuditActions';
+import Modal from '../Modal';
+import HouseAuditForm from './HouseAuditForm';
+
+// Render Modal
+const renderModalActions = () => {
   return (
-    <div>
-      <h1>HouseAuditEdit</h1>
-    </div>
+    <Fragment>
+      <button type="button" className="btn btn-secondary" data-dismiss="modal">
+        Close
+      </button>
+    </Fragment>
   );
 };
+const renderModal = () => {
+  return (
+    <Modal
+      title="Audit succesfully saved."
+      onDismiss={() => history.push('/houseaudits')}
+      actions={renderModalActions}
+    >
+      The Cottage audtid has been successfully saved!
+    </Modal>
+  );
+};
+// const formValues = {
+//   auditor: 'Nelson Abner Diaz',
+//   house: '31',
+//   department: 'UAC',
+//   date: '2019-07-01T04:00:00.000Z',
+//   items: {
+//     medication: {
+//       score: 10,
+//       findings: ['Finding 1', 'Finding 2']
+//     },
+//     ss: {
+//       score: 20
+//     },
+//     hc: {
+//       score: 20
+//     },
+//     documentation: {
+//       score: 10
+//     },
+//     organization: {
+//       score: 10
+//     },
+//     pr: {
+//       score: 10
+//     },
+//     supplies: {
+//       score: 5
+//     },
+//     equipment: {
+//       score: 0,
+//       findings: ['1 Chair is broken']
+//     }
+//   },
+//   score: 85
+// };
+class HouseAuditCreate extends Component {
+  componentDidMount() {
+    this.props.fetchHouseAudit(this.props.match.params.id);
+  }
 
-export default HouseAuditEdit;
+  onSubmit = formValues => {
+    this.props.editHouseAudit(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    console.log(this.props.match);
+    return (
+      <div className="container mt-5">
+        <h3 className="my-5">Edit House Audit ID: {this.props.match.params.id} </h3>
+        <HouseAuditForm onSubmit={this.onSubmit} values={this.props.houseAudit} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { houseAudit: state.houseAudits[ownProps.match.params.id] };
+};
+
+export default connect(
+  mapStateToProps,
+  { editHouseAudit, fetchHouseAudit }
+)(HouseAuditCreate);
---
+++ client/src/components/HouseAudits/HouseAuditForm.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { Form, Field } from 'react-final-form';
+import moment from 'moment';
+import arrayMutators from 'final-form-arrays';
+import createDecorator from 'final-form-calculate';
+import HouseAuditItem from './HouseAuditItem';
+
+// Data json files
+import { houses, houseAuditItems } from './houseAudits.json';
+
+const calculator = createDecorator({
+  field: /items.\w/,
+  updates: {
+    score: (ignoredValue, allValues) =>
+      (Object.values(allValues.items) || []).reduce(
+        (total, { score }) => total + Number(score || 0),
+        0
+      )
+  }
+});
+
+const HouseAuditForm = props => {
+  const onSubmit = values => {
+    props.onSubmit(values);
+  };
+
+  return (
+    <Form
+      onSubmit={onSubmit}
+      mutators={{
+        ...arrayMutators
+      }}
+      decorators={[calculator]}
+      initialValues={props.values || {}}
+    >
+      {({
+        handleSubmit,
+        form: {
+          mutators: { push }
+        }, // injected from final-form-arrays above
+        pristine,
+        form,
+        submitting,
+        values
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <label>Auditor</label>
+              <Field
+                className="form-control"
+                name="auditor"
+                component="input"
+                type="text"
+                placeholder="Auditor"
+              />
+            </div>
+
+            <div className="form-group col-md-2">
+              <label>House</label>
+              <Field className="custom-select" name="house" component="select">
+                <option selected>Select a house</option>
+                {houses.map(house => (
+                  <option key={house} value={house}>
+                    {house}
+                  </option>
+                ))}
+              </Field>
+            </div>
+            <div className="form-group col-md-2">
+              <label>Department</label>
+              <Field className="custom-select" name="department" component="select">
+                <option selected>Select a Dept</option>
+                <option value="Domestic">Domestic</option>
+                <option value="UAC">UAC</option>
+              </Field>
+            </div>
+            <div className="form-group col-md-4">
+              <label>Date</label>
+              <Field
+                className="form-control"
+                id="date"
+                name="date"
+                component="input"
+                type="date"
+                parse={value => moment(value)}
+                format={value => (value ? moment(value).format('YYYY-MM-DD') : '')}
+              />
+            </div>
+          </div>
+          {/* Audit Items */}
+          <div className="row my-5">
+            <h4 className="col-md-4">Audit Items</h4>
+            <label className="col-md-2 offset-md-4 mt-2 text-right">Score</label>
+            <div className="col-md-2">
+              <Field
+                className="form-control"
+                id="score"
+                name="score"
+                component="input"
+                readOnly
+                type="text"
+                placeholder="0"
+              />
+            </div>
+          </div>
+          {houseAuditItems.map((item, index) => (
+            <HouseAuditItem
+              key={index}
+              itemLabel={item.label}
+              itemName={`items.${item.name}`}
+              itemScores={item.scores}
+              itemMaxScore={item.max}
+              push={push}
+            />
+          ))}
+
+          <div className="buttons my-3">
+            <button
+              className="btn btn-primary mr-3"
+              type="submit"
+              disabled={submitting || pristine}
+            >
+              Submit
+            </button>
+            <button
+              className="btn btn-secondary mr-3"
+              type="button"
+              onClick={form.reset}
+              disabled={submitting || pristine}
+            >
+              Reset
+            </button>
+          </div>
+          {/* <pre>{JSON.stringify(values, 0, 2)}</pre> */}
+        </form>
+      )}
+    </Form>
+  );
+};
+
+export default HouseAuditForm;
