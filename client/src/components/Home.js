3b6724a348f9908b5c6afa68b2cd3edf154483c8
Nelson Diaz <abnerdc86@gmail.com>
2019-08-21 03:59:01 -0400

    add icons to the home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -26,99 +26,56 @@ const Home = () => {
       </div>
 
       {/* Three columns of text below the carousel */}
-      <div className="row">
-        <div className="col-lg-4">
-          <svg
-            className="bd-placeholder-img rounded-circle"
-            width="140"
-            height="140"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-            role="img"
-            aria-label="Placeholder: 140x140"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#777" />
-            <text x="50%" y="50%" fill="#777" dy=".3em">
-              140x140
-            </text>
-          </svg>
-          <h2>Heading</h2>
-          <p>
-            Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id
-            nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum
-            at eros. Praesent commodo cursus magna.
-          </p>
-          <p>
-            <a className="btn btn-secondary" href="#" role="button">
-              View details &raquo;
-            </a>
-          </p>
+      <div className="container">
+        <div className="row text-center mt-5">
+          <div className="col-lg-4">
+            <i className="fas fa-home fa-10x" />
+            <h2>House Audits</h2>
+            <p>
+              Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id
+              nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac,
+              vestibulum at eros. Praesent commodo cursus magna.
+            </p>
+            <p>
+              <a className="btn btn-secondary" href="#" role="button">
+                View details &raquo;
+              </a>
+            </p>
+          </div>
+          {/* /.col-lg-4 */}
+          <div className="col-lg-4">
+            <i className="fas fa-car fa-10x" />
+            <h2>Vehicle Audits</h2>
+            <p>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem
+              nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac
+              cursus commodo, tortor mauris condimentum nibh.
+            </p>
+            <p>
+              <a className="btn btn-secondary" href="#" role="button">
+                View details &raquo;
+              </a>
+            </p>
+          </div>
+          {/* /.col-lg-4 */}
+          <div className="col-lg-4">
+            <i className="fas fa-book fa-10x" />
+            <h2>Files Audits</h2>
+            <p>
+              Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+              Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus
+              commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+            </p>
+            <p>
+              <a className="btn btn-secondary" href="#" role="button">
+                View details &raquo;
+              </a>
+            </p>
+          </div>
+          {/* /.col-lg-4 */}
         </div>
-        {/* /.col-lg-4 */}
-        <div className="col-lg-4">
-          <svg
-            className="bd-placeholder-img rounded-circle"
-            width="140"
-            height="140"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-            role="img"
-            aria-label="Placeholder: 140x140"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#777" />
-            <text x="50%" y="50%" fill="#777" dy=".3em">
-              140x140
-            </text>
-          </svg>
-          <h2>Heading</h2>
-          <p>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem
-            nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac
-            cursus commodo, tortor mauris condimentum nibh.
-          </p>
-          <p>
-            <a className="btn btn-secondary" href="#" role="button">
-              View details &raquo;
-            </a>
-          </p>
-        </div>
-        {/* /.col-lg-4 */}
-        <div className="col-lg-4">
-          <svg
-            className="bd-placeholder-img rounded-circle"
-            width="140"
-            height="140"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-            role="img"
-            aria-label="Placeholder: 140x140"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#777" />
-            <text x="50%" y="50%" fill="#777" dy=".3em">
-              140x140
-            </text>
-          </svg>
-          <h2>Heading</h2>
-          <p>
-            Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-            Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus
-            commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-          </p>
-          <p>
-            <a className="btn btn-secondary" href="#" role="button">
-              View details &raquo;
-            </a>
-          </p>
-        </div>
-        {/* /.col-lg-4 */}
+        {/* /.row */}
       </div>
-      {/* /.row */}
     </div>
   );
 };
