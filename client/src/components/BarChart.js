fd91ac8f7b849ec558a7ea7b16a1abf83036654d
Nelson Diaz <abnerdc86@gmail.com>
2019-10-09 15:08:10 -0400

    sumarized data passed to chart

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { BarChart, Bar, LabelList, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  LabelList,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts';
 import { percentage } from '../utils/numbers';
 
 class HouseScoresChart extends Component {
@@ -16,29 +26,31 @@ class HouseScoresChart extends Component {
   render() {
     const { data } = this.props;
     return (
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 20,
-          right: 10,
-          left: 10,
-          bottom: 20
-        }}
-      >
-        {/* <CartesianGrid strokeDasharray="3 3" /> */}
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="name" fill="#007bff">
-          <LabelList dataKey="label" position="top" />
-        </Bar>
-        {/* <Bar dataKey="Maintenance" fill="#dc3545">
+      <div style={{ width: '100%', height: 300 }}>
+        <ResponsiveContainer>
+          <BarChart
+            data={data}
+            margin={{
+              top: 20,
+              right: 10,
+              left: 10,
+              bottom: 20
+            }}
+          >
+            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+            <XAxis dataKey="house" />
+            <YAxis />
+            <Tooltip />
+            {/* <Legend /> */}
+            <Bar dataKey="score" fill="#007bff">
+              <LabelList dataKey="label" position="top" />
+            </Bar>
+            {/* <Bar dataKey="Maintenance" fill="#dc3545">
           <LabelList dataKey="label" position="top" />
         </Bar> */}
-      </BarChart>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     );
   }
 }
--- client/src/components/HouseAudits/HouseAuditGeneralReport.js
+++ client/src/components/HouseAudits/HouseAuditGeneralReport.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Container, Row, Col, Table } from 'react-bootstrap';
 
 import { fetchHouseAudits } from '../../actions/houseAuditActions';
@@ -9,38 +9,39 @@ import moment from 'moment';
 import template from './houseAudits.json';
 import BarChart from '../BarChart';
 
-import { sortByParam } from '../../utils/numbers';
+import { sortByParam, percentage } from '../../utils/numbers';
 
 const data = [
   {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
+    house: 'House 22',
+    score: percentage(0.9)
   },
   {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
+    house: 'House 23',
+    score: percentage(0.5)
   },
   {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
+    house: 'House 25',
+    score: percentage(0.75)
   }
 ];
 
-class HouseAuditsGeneralReport extends Component {
-  componentDidMount() {
-    this.props.fetchHouseAudits();
-  }
-  renderRows = () => {
-    return !this.props.houseAudits ? (
+const HouseAuditsGeneralReport = props => {
+  const houseAudits = useSelector(state =>
+    sortByParam(Object.values(state.houseAudits), 'house', false)
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchHouseAudits());
+  }, []);
+
+  const renderRows = () => {
+    return !houseAudits ? (
       <Spinner />
     ) : (
-      this.props.houseAudits.map(audit => (
+      houseAudits.map(audit => (
         <tr key={audit._id}>
           <td>{moment(audit.date).format('MM/DD/YYYY')}</td>
           <td>{audit.house}</td>
@@ -54,50 +55,54 @@ class HouseAuditsGeneralReport extends Component {
     );
   };
 
-  render() {
-    return (
-      <Container fluid>
-        <div className="py-2" />
-        <h3 className="py-2">House Audits General Report</h3>
-        <div className="my-3">
-          <Link className="btn btn-primary" to="/houseaudits">
-            <i className="fas fa-chevron-left" /> Go back to List
-          </Link>
-        </div>
+  return (
+    <Container fluid>
+      <div className="py-2" />
+      <h3 className="py-2">House Audits General Report</h3>
+      <div className="my-3">
+        <Link className="btn btn-primary" to="/houseaudits">
+          <i className="fas fa-chevron-left" /> Go back to List
+        </Link>
+      </div>
 
-        <section className="chart my-3">
-          <BarChart data={data} />
-        </section>
+      <section className="chart my-3">
+        <BarChart data={sumarizeData(houseAudits)} />
+      </section>
 
-        <section className="mt-3">
-          <Table striped bordered hover size="sm">
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>House</th>
-                <th>Program</th>
-                {template.houseAuditItems.map((item, index) => (
-                  <th key={`th-${index}`}>{item.label}</th>
-                ))}
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>{this.renderRows()}</tbody>
-          </Table>
-        </section>
-      </Container>
-    );
-  }
-}
+      <section className="mt-3">
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>House</th>
+              <th>Program</th>
+              {template.houseAuditItems.map((item, index) => (
+                <th key={`th-${index}`}>{item.label}</th>
+              ))}
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>{renderRows()}</tbody>
+        </Table>
+      </section>
+      {/* <pre>{JSON.stringify(sumarizeData(houseAudits), '', 2)}</pre> */}
+    </Container>
+  );
+};
 
-const mapStateToProps = state => ({
-  app: state.app,
-  houseAudits: sortByParam(Object.values(state.houseAudits), 'house', false)
-});
+const sumarizeData = input => {
+  const { houses } = template;
+  let output = [];
+  houses.map(house => {
+    const filtered = input.filter(audit => audit.house === house);
+    const houseAVG = {
+      house: `House ${house}`,
+      score: filtered.reduce((acc, audit) => (acc + audit.score) / filtered.length, 0)
+    };
+    output.push(houseAVG);
+  });
 
-export default connect(
-  mapStateToProps,
-  {
-    fetchHouseAudits
-  }
-)(HouseAuditsGeneralReport);
+  return output;
+};
+
+export default HouseAuditsGeneralReport;
